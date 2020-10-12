3f8df69886328f42a2df26ba6299a4a9efc9a7cd
Michał Skrzypczyński <justflow555@gmail.com>
2020-10-12 18:17:26 +0200

    Add Select input option for each filter

---
+++ src/components/employeeFilterModal/Select.js
@@ -0,0 +1,169 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import Checkbox from '../inputs/Checkbox';
+import TextField from '../inputs/TextField';
+import { arrayEquals } from '../../helpers/helpersFunctions';
+
+const DropDownContainer = styled('div')`
+    width: 100%;
+    position: relative;
+    margin: 5px 0;
+    & ~ svg {
+        transform: ${(props) =>
+            props.open
+                ? `translateY(-50%) rotate(180deg)`
+                : `translateY(-50%) rotate(0deg)`} !important;
+    }
+`;
+
+const DropdownInputContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const DropDownListContainer = styled('div')``;
+
+const DropDownList = styled('ul')`
+    position: absolute;
+    width: 100%;
+    z-index: 100;
+    max-height: 150px;
+
+    overflow-y: scroll;
+    border: 1px solid #d0d0d0;
+    opacity: ${(props) => (props.isReady ? 1 : 0)};
+    transition: opacity 0.3s;
+    &::-webkit-scrollbar {
+        width: 10px;
+    }
+    &::-webkit-scrollbar-track {
+        background: #f1f1f1;
+    }
+    &::-webkit-scrollbar-thumb {
+        background: #d0d0d0;
+        transition: background 0.3s;
+    }
+    &::-webkit-scrollbar-thumb:hover {
+        background: #bebebe;
+    }
+`;
+
+const ListItem = styled('li')`
+    width: 100%;
+    list-style: none;
+    background-color: #fff;
+    border-bottom: 1px solid #bebebe;
+    height: 40px;
+    padding: 15px 5px;
+    display: flex;
+    align-items: center;
+    transition: opacity 0.3s;
+    &:last-child {
+        border-bottom: none;
+    }
+
+    opacity: ${(props) => (props.isOpen ? 0 : 1)};
+`;
+
+export default function Select({
+    items,
+    updateFilters,
+    selectedData,
+    setSelectedData,
+    id,
+    type,
+    selectAll,
+    isAllFiltersFilled = true,
+}) {
+    const [clickedOutside, setClickedOutside] = useState(true);
+    const myRef = useRef();
+
+    const handleClickOutside = (e) => {
+        if (!myRef.current.contains(e.target)) {
+            setClickedOutside(true);
+        }
+    };
+    const handleClickInside = () => setClickedOutside(false);
+
+    const renderSelectAllCheckbox = () => {
+        if (type === 'pracownicy' && items.length > 0) {
+            return (
+                <ListItem>
+                    <Checkbox
+                        label="Wszyscy"
+                        handleInputChange={selectAll}
+                        value={items}
+                        checked={arrayEquals(selectedData, items)}
+                        id={Math.random()}
+                    />
+                </ListItem>
+            );
+        }
+    };
+
+    const handleInputChange = (e) => {
+        let checked = e.target.checked;
+        let checkedValue = e.target.value;
+        let selectedFilters;
+        if (checked) {
+            selectedFilters = [...selectedData, checkedValue];
+        } else {
+            selectedFilters = selectedFilters.filter(
+                (selectedFilter) => selectedFilter !== checkedValue
+            );
+        }
+        setSelectedData(selectedFilters);
+        if (type !== 'pracownicy') updateFilters(type, selectedFilters);
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () =>
+            document.removeEventListener('mousedown', handleClickOutside);
+    });
+
+    return (
+        <DropDownContainer
+            ref={myRef}
+            onClick={handleClickInside}
+            open={!clickedOutside && isAllFiltersFilled}
+        >
+            <DropdownInputContainer>
+                <TextField
+                    selectedData={selectedData}
+                    clickedOutside={clickedOutside}
+                    id={id}
+                    labelText={type}
+                    isAllFiltersFilled={isAllFiltersFilled}
+                />
+            </DropdownInputContainer>
+
+            {!clickedOutside && (
+                <DropDownListContainer>
+                    <DropDownList isReady={items.length > 0}>
+                        {renderSelectAllCheckbox()}
+                        {items.map((item, index) => {
+                            return (
+                                <ListItem
+                                    isOpen={clickedOutside}
+                                    key={Math.random()}
+                                    id={id}
+                                >
+                                    <Checkbox
+                                        label={item}
+                                        handleInputChange={handleInputChange}
+                                        value={item}
+                                        checked={selectedData.includes(item)}
+                                        id={item + '_' + index}
+                                    />
+                                </ListItem>
+                            );
+                        })}
+                    </DropDownList>
+                </DropDownListContainer>
+            )}
+        </DropDownContainer>
+    );
+}
