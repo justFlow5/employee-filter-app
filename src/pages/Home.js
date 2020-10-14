4320e0473b7b2a0733c275c7ed55073e92dd91c1
Michał Skrzypczyński <justflow555@gmail.com>
2020-10-14 23:32:14 +0200

    Remove redundant state update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -204,7 +204,6 @@ function Home({ config }) {
         } else {
             setIsAllFiltersFilled(false);
             setFilteredWorkers([]);
-            setSelectedWorkers([]);
         }
     }, [
         selectedPositions,
